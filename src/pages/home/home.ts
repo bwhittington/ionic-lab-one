b8b6ff1b5c3ac898a32b57efe469a8f01549718f
Brett Whittington <BWhittington@hmbnet.com>
2018-04-28 15:35:50 -0400

    Commit solution

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LabOnePage } from '../lab-one/lab-one';
 
 @Component({
   selector: 'page-home',
@@ -11,4 +12,7 @@ export class HomePage {
 
   }
 
+  navigateTo(){
+    this.navCtrl.push(LabOnePage)
+  }
 }
